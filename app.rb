81c9a1b1018273b8df08a09705e500e0b922df33
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-28 17:25:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analyzed_text = TextAnalyzer.new(params[:user_text]).string 
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
-    erb :results
+    erb :results.string
   end
 end
